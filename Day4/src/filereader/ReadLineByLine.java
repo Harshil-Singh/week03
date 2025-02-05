ac4b73b3eb0ff39cdaa514c354462c3dec37fddb
Your Name <harshil0412singh@gmail.com>
2025-02-05 10:26:00 +0530

    harshil branch created

--- Day4/src/filereader/ReadLineByLine.java
+++ Day4/src/filereader/ReadLineByLine.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class ReadLineByLine {
     public static void main(String[] args){
-        String filepath = "E:\\Capg\\Week3\\Day4\\src\\filereader\\file.txt";
+        String filepath = "C:\\Users\\j1970\\Desktop\\codeex\\programming practices week 3\\Day4\\src\\filereader\\file.txt";
         try(BufferedReader bf = new BufferedReader(new FileReader(filepath))){
             String line;
             while((line=bf.readLine()) != null){
